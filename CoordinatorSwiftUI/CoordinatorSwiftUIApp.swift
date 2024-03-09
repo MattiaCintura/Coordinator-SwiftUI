2e4094b1398a3f73f8dacd46cbbf83e953d2b9cc
Mattia Cintura <mattiacintura@icloud.com>
2024-03-09 15:03:37 +0100

    Initial Commit

---
+++ CoordinatorSwiftUI/CoordinatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorSwiftUIApp.swift
+//  CoordinatorSwiftUI
+//
+//  Created by Mattia Cintura on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
